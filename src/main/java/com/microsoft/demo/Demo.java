60390affa4de265b4a22c66e75189691a75a6df1
umeshwaghode <77615542+umeshwaghode@users.noreply.github.com>
2021-04-07 11:23:49 +0530

    Update Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -3,6 +3,7 @@ package com.microsoft.demo;
 public class Demo {
     
     public String password = "Hello";
+    public String SQLQ = "Select * from mytable where name=?";
     public void DoSomething(boolean flag){
         if(flag){
             System.out.println("I ama covered");
